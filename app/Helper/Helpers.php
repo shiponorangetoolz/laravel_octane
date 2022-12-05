d8c362e63a149284aa49982c0e0b2c91a4687b29
Shipon <shipon@orangetoolz.com>
2022-12-05 16:38:58 +0600

    dispatcher

--- app/Helper/Helpers.php
+++ app/Helper/Helpers.php
@@ -44,57 +44,5 @@ class Helpers
         $db = new DbHelpers();
         $db->insert($insertData);
     }
-
-    public static function dispatchRunner(array $globalContact = [], array $itemsData = [], $batch)
-    {
-        $insertData = [];
-//
-//        $item = array_chunk($itemsData, 100);
-//
-//        $one = $item[0] ?? [];
-//        $two = $item[1] ?? [];
-
-        \yidas\WorkerDispatcher::run([
-            'debug' => true,
-            'workers' => 2,
-            'config' => ['uri' => "/v1/resource"],
-            'tasks' => false,
-            'callbacks' => [
-                'process' => function ($config, $workerId, $task) use ($insertData, $itemsData) {
-
-                    for ($i = 1; $i < $itemsData; $i++) {
-                        $contact = 100000000 . "" . $i;
-
-                        $user = User::where(['id' => $i])->first();
-
-                        if (!is_null($user)) {
-                            $name = $user->name;
-                        }
-
-                        $db = new DbHelpers();
-
-//                        $db->curlRequest();
-
-                        $duplicate = $db->checkDuplicate($contact);
-                        echo $duplicate;
-                        if (is_null($duplicate)) {
-                            $insertData[] = [
-                                'uid' => "uid" . $i,
-                                'contact' => $contact
-                            ];
-                        }
-
-
-                    }
-                    print_r($insertData);
-
-                    $db = new DbHelpers();
-                    $db->insert($insertData);
-                },
-            ],
-        ]);
-
-
-    }
 }
 
